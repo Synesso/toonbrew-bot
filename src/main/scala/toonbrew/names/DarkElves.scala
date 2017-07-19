624b4a861eb1803a43737103faca071153a39f4f
Jem Mawson <jem.mawson@gmail.com>
2017-07-19 11:03:53 +1000

    fix de

--- src/main/scala/toonbrew/names/DarkElves.scala
+++ src/main/scala/toonbrew/names/DarkElves.scala
@@ -1,18 +1,16 @@
 package toonbrew.names
 
-import toonbrew.Enums.Gender
-
-import scala.util.Random
+import toonbrew.Enums.{Female, Gender, Male}
 
 object DarkElves extends Names {
 
-  private val generators: Seq[() => String] = List(warhammer _)
+  private val generators: Seq[(Gender) => String] = List(warhammer _)
 
   def name(gender: Gender): String = {
-    choose(generators)()
+    choose(generators)(gender)
   }
 
-  private def warhammer = {
+  private def warhammer(gender: Gender) = {
     val nm1 = List("c", "d", "g", "k", "l", "m", "n", "q", "r", "t", "v")
     val nm2 = List("a", "e", "i", "o", "u", "a", "e", "i", "o", "u", "a", "e", "i", "o", "u", "ou", "au")
     val nm3 = List("c", "cc", "cr", "ch", "g", "gh", "gr", "gn", "k", "kh", "kr", "kk", "kz", "l", "ll", "lk", "lc", "lg", "n", "nn", "nk", "r", "rv", "rk", "rc", "rg", "rz", "rl", "tr", "th", "vr", "v", "c", "g", "k", "l", "n", "r", "v", "c", "g", "k", "l", "n", "r", "v", "c", "g", "k", "l", "n", "r", "v")
@@ -21,16 +19,17 @@ object DarkElves extends Names {
     val nm6 = List("a", "e", "i", "o")
     val nm7 = List("b", "bh", "c", "ch", "gh", "gg", "h", "hh", "kh", "l", "ll", "lr", "ln", "lv", "r", "rr", "rt", "rl", "rs", "rn", "rv", "s", "ss", "sh", "t", "tt", "th", "v", "vh", "b", "c", "h", "l", "r", "s", "t", "v", "kh", "b", "c", "h", "l", "r", "s", "t", "v", "kh")
     val nm8 = List("h", "n", "l", "sh", "s", "th", "", "", "", "", "")
-    val nm9 = List("amber", "ash", "battle", "blood", "cinder", "dark", "dawn", "dead", "death", "doom", "dread", "dusk", "dust", "ember", "fall", "fallen", "fell", "fire", "flame", "gloom", "grim", "haze", "hell", "nether", "night", "pyre", "rage", "rain", "shade", "shadow", "silent", "skull", "steel", "storm", "thunder", "void", "war", "wild")
-    val nm10 = List("arm", "arrow", "axe", "bane", "basher", "binder", "blade", "blaze", "bleeder", "blight", "breaker", "bringer", "caller", "cleaver", "crusher", "cutter", "eye", "eyes", "fall", "fury", "grip", "hand", "heart", "hunter", "mantle", "maul", "might", "more", "reaper", "reaver", "rider", "ripper", "runner", "scar", "seeker", "shade", "shadow", "shard", "slayer", "sorrow", "stalker", "stride", "strike", "striker", "surge", "taker")
 
-    val i = Random.nextInt(10)
-    val rnd = Random.nextInt(nm9.size)
-    val rnd2 = Random.nextInt(nm10.size)
-    val nameL = nm9(rnd) + nm10(rnd2)
-
-    nameL
+    gender match {
+      case Female => choose(List(
+        () => s"${choose(nm5)}${choose(nm6)}${choose(nm7)}${choose(nm6)}${choose(nm8)}",
+        () => s"${choose(nm5)}${choose(nm6)}${choose(nm7)}${choose(nm6)}${choose(nm7)}${choose(nm6)}${choose(nm8)}"
+      ))()
+      case Male => choose(List(
+          () => s"${choose(nm1)}${choose(nm2)}${choose(nm3)}${choose(nm2)}${choose(nm4)}",
+          () => s"${choose(nm1)}${choose(nm2)}${choose(nm3)}${choose(nm2)}${choose(nm3)}${choose(nm2)}${choose(nm4)}"
+        ))()
+    }
   }
-  
-  
+
 }
